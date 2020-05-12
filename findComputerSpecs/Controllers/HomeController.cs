7cbb703ebb39311685aabb25253724fcbb006b7d
unknown <i.p.m.a.n.03.31.87@gmail.com>
2020-05-12 15:47:48 -0500

    first commit

---
+++ findComputerSpecs/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace findComputerSpecs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View();
+
+        public IActionResult BriefSpecs() => View();
+
+        public IActionResult DetailedSpecs() => View();
+
+        public ViewResult MainPage() => View("Index");
+               
+    }
+}
---
+++ findComputerSpecs/Models/ProcessorDetailedInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Threading.Tasks;
+
+
+namespace findComputerSpecs.Models
+{
+    public class ProcessorDetailedInfo
+    {
+    //    ManagementObjectSearcher myProcessorObject = new ManagementObjectSearcher("select * from Win32_Processor");
+
+    //        foreach (ManagementObject obj in myProcessorObject.Get())
+    //        {
+    //            Console.WriteLine("Name  -  " + obj["Name"]);
+    //            Console.WriteLine("DeviceID  -  " + obj["DeviceID"]);
+    //            Console.WriteLine("Manufacturer  -  " + obj["Manufacturer"]);
+    //            Console.WriteLine("CurrentClockSpeed  -  " + obj["CurrentClockSpeed"]);
+    //            Console.WriteLine("Caption  -  " + obj["Caption"]);
+    //            Console.WriteLine("NumberOfCores  -  " + obj["NumberOfCores"]);
+    //            Console.WriteLine("NumberOfLogicalProcessors  -  " + obj["NumberOfLogicalProcessors"]);
+    //            Console.WriteLine("Architecture  -  " + obj["Architecture"]);
+    //            Console.WriteLine("Family  -  " + obj["Family"]);
+    //            Console.WriteLine("ProcessorType  -  " + obj["ProcessorType"]);
+    //            Console.WriteLine("AddressWidth  -  " + obj["AddressWidth"]);
+    //        }
+    //Console.ReadKey();
+
+        
+    }
+  
+}
